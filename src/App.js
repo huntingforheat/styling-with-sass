e978538b97f85588346951d8076203b3ea5f7489
huntingforheat <asktt1234@gmail.com>
2024-06-14 17:17:43 +0900

    240517

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from './component/Button';
+import Box from './cssmodule/Box';
+import CheckBox from './cssmodule/component/CheckBox';
+import { useState } from 'react';
 
 function App() {
+
+  const [check, setCheck] = useState(false);
+  const onChange = e => {
+    setCheck(e.target.checked);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Sass 테스트 */}
+      {/* <div className='button'>
+        <Button size="large" onClick={() => alert('클릭했어요')}>Button</Button>
+        <Button>Button</Button>
+        <Button size={"small"}>Button</Button>
+      </div>
+      <div className='button'>
+        <Button size="large" color={"gray"}>Button</Button>
+        <Button color={"gray"}>Button</Button>
+        <Button size={"small"} color={"gray"}>Button</Button>
+      </div>
+      <div className='button'>
+        <Button size="large" color={"pink"}>Button</Button>
+        <Button color={"pink"}>Button</Button>
+        <Button size={"small"} color={"pink"}>Button</Button>
+      </div>
+      <div className='button'>
+        <Button size="large" color={"blue"} fullWidth>Button</Button>
+        <Button color={"gray"} fullWidth>Button</Button>
+        <Button size={"small"} color={"pink"} fullWidth>Button</Button>
+      </div> */}
+
+      {/* CSS Module 테스트 */}
+      {/* <Box/>
+      <Box/> */}
+      <CheckBox onChange={onChange} checked={check}>
+        다음 약관에 모두 동의
+      </CheckBox>
+      <p>
+        <b>check : </b>
+        { check ? 'true':'false'}
+      </p>
     </div>
   );
 }
---
+++ src/component/Button.js
@@ -0,0 +1,29 @@
+// Button
+import React from 'react';
+import classNames from 'classnames';
+import './Button.scss';
+// 1. 버튼생성
+// 2. 버튼 사이즈 조정하기 (large, medium, small)
+// 3. 버튼 색상 설정하기 (blue, gray, pink)
+// 4. outline 옵션 : 버튼에서 테두리만 보여지게 설정
+// 5. fullWidth : 버튼이 전체 너비를 차지하도록 구현
+function Button({children, size, color, outline, fullWidth, ...rest}) {
+    // className='Button large'
+    // ['Button', size].join(' ') or `Button ${size}`
+    // 클래스에 관련된 처리를 쉽게하는 라이브러리, classnames
+    return (
+        <button 
+            className={classNames('Button', size, color, {outline, fullWidth})}
+            {...rest}
+        >
+            {children}
+        </button>
+    );
+}
+
+Button.defaultProps = {
+    size: 'medium',
+    color: 'blue'
+};
+
+export default Button;
